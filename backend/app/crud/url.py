094b024e02f4d536e87ada3674a4881e039049e2
cburke6897 <cburke6897@gmail.com>
2026-02-12 12:06:22 -0500

    Adds url.py in backend/app/crud

---
+++ backend/app/crud/url.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from app.models.url import URL
+
+def get_url_by_code(db: Session, code: str):
+    return db.query(URL).filter(URL.code == code).first()
+
+def create(db: Session, original_url: str, code: str):
+    db_url = URL(code=code, original_url=original_url)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
