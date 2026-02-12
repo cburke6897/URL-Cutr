8411dd1ce9997c84dd4175aa36d192e06e36b436
cburke6897 <cburke6897@gmail.com>
2026-02-12 16:14:07 -0500

    Creates postgres table

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.db.session import engine
+from app.db.base import Base
 from app.api.routes.shorten import router as shorten_router
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 app.include_router(shorten_router)
