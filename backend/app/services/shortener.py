20b13f21ea7b2077bc1246db880b6a885dd9052b
cburke6897 <cburke6897@gmail.com>
2026-02-12 12:05:13 -0500

    Renames generate_short_code to generate_code

--- backend/app/services/shortener.py
+++ backend/app/services/shortener.py
@@ -3,5 +3,5 @@ from nanoid import generate
 
 characters = string.ascii_letters + string.digits
 
-def generate_short_code(length=6):
+def generate_code(length=6):
     return generate(characters, length)
