590f71e6e54d5e9a751abd3977c0b532ac11b071
cburke6897 <cburke6897@gmail.com>
2026-02-12 19:09:44 -0500

    Adds CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine
 from app.db.base import Base
-from app.api.routes.shorten import router as shorten_router
+from backend.app.api.routes import router as shorten_router
 
 app = FastAPI()
 
 # Create database tables based on the defined models
 Base.metadata.create_all(bind=engine)
 app.include_router(shorten_router)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
