fc1a927348def7b6c023951722e4a38a41f69ae8
cburke6897 <cburke6897@gmail.com>
2026-02-13 17:40:20 -0500

    Loads all valid TLDs on startup using tlds.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine
 from app.db.base import Base
 from app.api.routes.routes import router as shorten_router
+from app.core.tlds import load_tlds
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load valid TLDs at startup
+    await load_tlds()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 
 # Create database tables based on the defined models
 Base.metadata.create_all(bind=engine)
