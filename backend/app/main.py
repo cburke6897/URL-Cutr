28c1b3c07afd18ab193f42f6bbf45d3ad8dad7e5
cburke6897 <cburke6897@gmail.com>
2026-02-13 20:33:58 -0500

    Deletes expired urls after a set period of time in a loop

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
@@ -5,19 +6,21 @@ from app.db.session import engine
 from app.db.base import Base
 from app.api.routes.routes import router as shorten_router
 from app.core.tlds import load_tlds
+from app.services.cleanup import delete_expired_urls_periodically
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load valid TLDs at startup
     await load_tlds()
+    # Start the periodic cleanup task to delete expired URLs
+    asyncio.create_task(delete_expired_urls_periodically())
+    # Create database tables based on the defined models
+    Base.metadata.create_all(bind=engine)
     yield
 
 app = FastAPI(lifespan=lifespan)
 
-
-# Create database tables based on the defined models
-Base.metadata.create_all(bind=engine)
 app.include_router(shorten_router)
 
 # Add CORS middleware
