e6cc36e7ac0cfcbcbc4bc3be1b3f95357e4e685c
cburke6897 <cburke6897@gmail.com>
2026-02-16 09:19:57 -0500

    Creates user_model.py in backend/app/models

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, String, DateTime, BigInteger, Boolean
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    admin = Column(Boolean, default=False)
