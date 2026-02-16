2784d4e270b4c2537e6369322146ae8ddce2b8e8
cburke6897 <cburke6897@gmail.com>
2026-02-16 09:25:03 -0500

    Creates user_crud.py in backend/app/crud

---
+++ backend/app/crud/user_crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models.user_model import User
+from app.core.security import hash_password
+
+
+# Create a new user entry in the database
+def create(db: Session, username: str, email: str, password: str):
+    hashed_password = hash_password(password)
+
+    db_user = User(username=username, email=email, hashed_password=hashed_password)
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
