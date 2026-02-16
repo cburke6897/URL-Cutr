a1ec01fd76007b83cd712685744759516fdc06db
cburke6897 <cburke6897@gmail.com>
2026-02-16 09:24:44 -0500

    Creates security.py in backend/app/core

---
+++ backend/app/core/security.py
@@ -0,0 +1,21 @@
+from app.core.config import settings
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from jose import jwt
+
+SECRET_KEY = settings.secret_key
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
