7cec868e3136ed4886115b35af94591d79e5d77b
cburke6897 <cburke6897@gmail.com>
2026-02-17 15:00:15 -0500

    Adds UserLogin payload as an argument to login route

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.cruds import user_crud
-from app.schemas.user_schema import UserCreate
+from app.schemas.user_schema import UserCreate, UserLogin
 
 router = APIRouter()
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
-    access_token = user_crud.login(db, email, password)
+def login(payload: UserLogin, db: Session = Depends(get_db)):
+    access_token = user_crud.login(db, payload.email, payload.password)
     if not access_token:
         raise HTTPException(status_code=400, detail="Invalid email or password")
     return {"access_token": access_token, "token_type": "bearer"}
