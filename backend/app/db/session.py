d286b5315b2f996dd2762cb6f87b02e9f9b5e42a
cburke6897 <cburke6897@gmail.com>
2026-02-16 20:11:37 -0500

    Adds get_db method

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,3 +4,11 @@ from app.core.config import settings
 
 engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Retrieves a database session for each request and ensures it is closed after the request is processed
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
