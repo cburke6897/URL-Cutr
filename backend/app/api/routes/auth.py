6849631f63d6c387a8b9ccbb337c93b3163232db
cburke6897 <cburke6897@gmail.com>
2026-02-17 14:43:42 -0500

    Makes singup route take UserCreate as a parameter

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.cruds import user_crud
+from app.schemas.user_schema import UserCreate
 
 router = APIRouter()
 
@@ -13,8 +14,8 @@ def login(email: str, password: str, db: Session = Depends(get_db)):
     return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/signup")
-def signup(email: str, username: str, password: str, db: Session = Depends(get_db)):
-    user = user_crud.create(db, email, username, password)
+def signup(payload: UserCreate, db: Session = Depends(get_db)):
+    user = user_crud.create(db, payload.email, payload.username, payload.password)
     if not user:
         raise HTTPException(status_code=400, detail="Email or username already registered")
     return {"message": "User created successfully"}
