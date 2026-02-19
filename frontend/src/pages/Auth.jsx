bee59019911bdf0eb6335b6da3632eb90fc8d6e2
cburke6897 <cburke6897@gmail.com>
2026-02-19 11:28:15 -0500

    Moves methods for refresh tokens to utils/RefreshToken.js

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -143,54 +143,6 @@ export default function Auth() {
         }
     };
 
-    async function refreshAccessToken() { // Function to refresh access token using refresh token stored in HTTP-only cookie
-        const response = await fetch("http://localhost:8000/refresh", {
-            method: "POST",
-            credentials: "include",
-        });
-
-        if (!response.ok) {
-            console.error("Failed to refresh token:", response.status);
-            localStorage.removeItem("token");
-            return;
-        }
-
-        const data = await response.json();
-        localStorage.setItem("token", data.access_token);
-        return data.access_token;
-    }
-
-    async function authFetch(url, options = {}) { // Wrapper around fetch to automatically include access token and handle 401 responses by attempting to refresh the token
-        const token = localStorage.getItem("token");
-
-        const response = await fetch(url, {
-            ...options,
-            headers: {
-                ...(options.headers || {}),
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
-        
-        if (response.status === 401) {
-            const newToken = await refreshAccessToken();
-            if (!newToken) { return response; }
-
-            return fetch(url, {
-            ...options,
-            headers: {
-                ...(options.headers || {}),
-                Authorization: `Bearer ${newToken}`,
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            });
-        }
-
-        return response;
-    }
-
     return (
         <div className="min-h-screen flex items-center justify-center bg-[var(--color-bg-light)] dark:bg-[var(--color-bg-dark)] transition-colors p-4">
         <DropdownMenu />
