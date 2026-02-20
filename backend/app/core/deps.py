33414511a6e8172e7b2900b443a06d3a099facb1
cburke6897 <cburke6897@gmail.com>
2026-02-20 12:24:42 -0500

    Creates get_current_user_optional method

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -6,8 +6,9 @@ from app.db.session import get_db
 from app.cruds.user_crud import get_user_by_id
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme_optional = OAuth2PasswordBearer(tokenUrl="/login", auto_error=False)
 
-def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+def get_current_user(token: str = Depends(oauth2_scheme), db = Depends(get_db)):
     try:
         payload = jwt.decode(token, ACCESS_TOKEN_SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
@@ -18,4 +19,17 @@ def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
     if not user:
         raise HTTPException(status_code=401, detail="User not found")
 
+    return user
+
+def get_current_user_optional(token: str = Depends(oauth2_scheme_optional), db = Depends(get_db)):
+    if not token:
+        return None
+
+    try:
+        payload = jwt.decode(token, ACCESS_TOKEN_SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+    except JWTError:
+        return None
+
+    user = get_user_by_id(db, user_id)
     return user
