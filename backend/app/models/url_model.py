8993a8bb1bfd3aa8c49c1b4d40399df4edf62d55
cburke6897 <cburke6897@gmail.com>
2026-02-20 12:33:55 -0500

    Replaces datetime.now() with sqlalchemy.fun.now()

--- backend/app/models/url_model.py
+++ backend/app/models/url_model.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timezone
-from sqlalchemy import Column, Integer, String, DateTime, BigInteger
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, func
 from app.db.base import Base
 
 class URL(Base):
@@ -8,7 +7,7 @@ class URL(Base):
     id = Column(BigInteger, primary_key=True, index=True)
     code = Column(String, unique=True, index=True)
     original_url = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    created_at = Column(DateTime(timezone=True), default=func.now())
     created_by = Column(BigInteger, nullable=True)
     delete_at = Column(DateTime(timezone=True), nullable=True)
     clicks = Column(Integer, default=0)
