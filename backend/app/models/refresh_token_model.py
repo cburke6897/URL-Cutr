a39ab72e8c02733ac0dcca88c6e1d3d7b276f0ac
cburke6897 <cburke6897@gmail.com>
2026-02-19 21:09:52 -0500

    Makes id type BigInteger

--- backend/app/models/refresh_token_model.py
+++ backend/app/models/refresh_token_model.py
@@ -1,14 +1,14 @@
 
 from app.db.base import Base
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 
 class RefreshToken(Base):
     __tablename__ = "refresh_tokens"
     
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    id = Column(BigInteger, primary_key=True, index=True)
+    user_id = Column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     token = Column(String, unique=True, index=True, nullable=False)
     expires_at = Column(DateTime, nullable=False)
 
