77f9d0f8e32a646f1d07142f088609a72a0b0171
cburke6897 <cburke6897@gmail.com>
2026-02-22 22:48:37 -0500

    Adds /dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
