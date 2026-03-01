1b08205017997003c7e5fd92eeaa0bad2a098f68
cburke6897 <cburke6897@gmail.com>
2026-02-28 19:16:18 -0500

    Adds password-reset route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/PasswordReset";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/password-reset" element={<ForgotPassword />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
