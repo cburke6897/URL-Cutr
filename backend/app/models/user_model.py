01c1baeb70e61ced8be354de31fb1e9944ece8fc
cburke6897 <cburke6897@gmail.com>
2026-03-01 17:15:05 -0500

    Replaces hash_password and verify_password  with hash_string and verify_string

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, DateTime, BigInteger, Boolean, func
 from app.db.base import Base
 from fastapi import HTTPException
-from app.core.security import hash_password, verify_password
+from app.core.security import hash_string, verify_string
 
 class User(Base):
     __tablename__ = "users"
@@ -14,7 +14,7 @@ class User(Base):
     admin = Column(Boolean, default=False)
     
     def set_password(self, plain_password: str):
-        if(verify_password(plain_password, self.hashed_password)):
+        if(verify_string(plain_password, self.hashed_password)):
             raise HTTPException(status_code=400, detail="New password must be different from the current password")
 
-        self.hashed_password = hash_password(plain_password)
+        self.hashed_password = hash_string(plain_password)
