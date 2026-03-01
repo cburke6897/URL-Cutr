8e14c731498abac64006aed4b9157aaca231bf09
cburke6897 <cburke6897@gmail.com>
2026-03-01 17:08:54 -0500

    Creates delete_token_model

---
+++ backend/app/models/delete_token_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, DateTime, BigInteger, func
+from app.db.base import Base
+
+class DeleteToken(Base):
+    __tablename__ = "delete_tokens"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    user_email = Column(String, nullable=False)
+    token = Column(String, unique=True, index=True, nullable=False)
+    expires_at = Column(DateTime(timezone=True), nullable=False)
+    created_at = Column(DateTime(timezone=True), default= func.now())
