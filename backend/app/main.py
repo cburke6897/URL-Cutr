8d05b9e067730004e591a197f63580630c51e522
cburke6897 <cburke6897@gmail.com>
2026-03-02 19:35:07 -0500

    Sets allowed origins to frontend_url

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ from app.api.routes.url_routes import router as shorten_router
 from app.api.routes.auth_routes import router as auth_router
 from app.api.routes.reset_routes import router as reset_router
 from app.api.routes.delete_routes import router as delete_router
+from app.core.config import settings
 from app.core.tlds import load_tlds
 from app.services.cleanup import delete_expired_urls_periodically
 
@@ -29,11 +30,18 @@ app.include_router(auth_router)
 app.include_router(reset_router)
 app.include_router(delete_router)
 
+def add_www(url: str) -> str:
+    if "://www." in url:
+        return url
+    return url.replace("://", "://www.", 1)
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        settings.frontend_url,
+        add_www(settings.frontend_url)
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
