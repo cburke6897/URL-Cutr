0e72eca092810cbd3c1903b8e283a4ae902af6b0
Christopher Burke <cburke6897@gmail.com>
2026-03-05 21:59:15 +0000

    Makes redis optional

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 # Configuration class to load settings from environment variables
 class Settings(BaseSettings):
     database_url: str
-    redis_url: str
+    redis_url: str | None = None
     access_token_secret_key: str
     refresh_token_secret_key: str
     resend_api_key: str
