924aedd10131afdeb2f76cdffe62da1276941492
Christopher Burke <cburke6897@gmail.com>
2026-03-06 03:37:54 +0000

    Imports base_class

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,3 +1,4 @@
+from app.db.base_class import Base
 # Import models so they are registered on Base.metadata for Alembic autogenerate.
 from app.models.delete_token_model import DeleteToken
 from app.models.refresh_token_model import RefreshToken
