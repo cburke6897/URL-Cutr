e54897f80e788a93d565ffce80b52c51c5040df6
Christopher Burke <cburke6897@gmail.com>
2026-03-06 04:21:09 +0000

    Makes urls that don't expire bypass time checks

--- backend/app/cruds/url_crud.py
+++ backend/app/cruds/url_crud.py
@@ -4,11 +4,11 @@ from app.models.url_model import URL
 
 # Retrieve a URL entry from the database based on the provided code
 def get_url_by_code(db: Session, code: str):
-    return db.query(URL).filter(URL.code == code, URL.delete_at > datetime.now(timezone.utc)).first()
+    return db.query(URL).filter(URL.code == code, URL.delete_at > datetime.now(timezone.utc) or URL.delete_at.is_(None)).first()
 
 # Retrieve all URL entries from the database for a specific user
 def get_urls_by_user(db: Session, user_id: int):
-    return db.query(URL).filter(URL.created_by == user_id, URL.delete_at > datetime.now(timezone.utc)).order_by(URL.created_at.desc()).all()
+    return db.query(URL).filter(URL.created_by == user_id, URL.delete_at > datetime.now(timezone.utc) or URL.delete_at.is_(None)).order_by(URL.created_at.desc()).all()
 
 # Create a new URL entry in the database
 def create(db: Session, original_url: str, code: str, delete_after: int, created_by: int | None = None):
